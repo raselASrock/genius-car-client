46cc73c7eeb05672d47df51a7e8f38aeceb5179e
raSel <mdraselm413@gmail.com>
2022-11-01 23:59:58 +0600

    data

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import img from  '../../assets/images/login/login.svg'
+import { AuthContext } from "../../contexts/Authprovider/AuthProvider";
 
 const SignUp = () => {
 
+    const {createUser} = useContext(AuthContext);
+
     const handleSignUp = event =>{
         event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
     }
     
   return (
